7bc56320876911f598d917560c1c0a39a0314cec
oktaneblu <45483943+oktaneblu@users.noreply.github.com>
2024-02-22 13:58:19 -0800

    Update win_RecentDownloads.ps1

--- win_RecentDownloads.ps1
+++ win_RecentDownloads.ps1
@@ -1,9 +1,13 @@
-#Last 4 Hours of Downloaded Files - VersionInfo + LastWriteTimeUtc + Zone.Identifier - Most Recent First
 $d = @(Get-ChildItem C:\Users\*\Downloads\*.* -Force -EA 0 | ?{$_.LastWriteTimeUtc -gt [DateTime]::UtcNow.AddHours(-4)} | Sort LastWriteTimeUtc -Desc)
-ForEach ($i in $d) {
-    Write-Output ''
-    'LastWriteTimeUtc : '+$i.LastWriteTimeUtc.DateTime
-    $i.VersionInfo | Select FileName,OriginalFileName,InternalName,FileDescription,Productname,CompanyName,Comments
-    Get-Content $i.FullName -Stream Zone.Identifier -Force -EA 0
-    Write-Output ''
-    }
+if (!$d) {
+    Write-Output '[-] No files written to downloads over the last 4 hours.'
+}
+else {
+    ForEach ($i in $d) {
+        Write-Output ''
+        'LastWriteTimeUtc : '+$i.LastWriteTimeUtc.DateTime
+        $i.VersionInfo | Select FileName,OriginalFileName,InternalName,FileDescription,Productname,CompanyName,Comments
+        Get-Content $i.FullName -Stream Zone.Identifier -Force -EA 0
+        Write-Output ''
+        }
+}
